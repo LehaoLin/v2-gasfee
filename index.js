83bafe741a3846f58417f2664adedad9804e498f
LehaoLin <lehaolin@link.cuhk.edu.cn>
2021-12-08 22:42:11 +0800

    update

--- index.js
+++ index.js
@@ -11,33 +11,40 @@ function sleep (time) {
 (async () => {
 
     while (1){
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto('https://crypto.com/defi/dashboard/gas-fees', { timeout: 0 });
+
+        try{
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://crypto.com/defi/dashboard/gas-fees', { timeout: 0 });
+        
+            let current = {}
+            let now = dayjs()
+            current.time = now.unix()
+        
+            let target = await page.$x(`//*[@id="__next"]/div/div/div/div/main/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[7]`)
     
-        let current = {}
-        let now = dayjs()
-        current.time = now.unix()
+            current.price = await page.evaluate(el => el.textContent, target[0])
     
-        let target = await page.$x(`//*[@id="__next"]/div/div/div/div/main/div[2]/div[2]/div[1]/div/table/tbody/tr[6]/td[7]`)
-
-        current.price = await page.evaluate(el => el.textContent, target[0])
-
-        var file_path = path.join(__dirname, 'data.json'); 
-
-        if (fs.existsSync(file_path)){
-            var data_pre = JSON.parse(fs.readFileSync(file_path))
-            data_pre.push(current)
-            fs.unlinkSync(file_path)
-        }else{
-            var data_pre = [current]
+            var file_path = path.join(__dirname, 'data.json'); 
+    
+            if (fs.existsSync(file_path)){
+                var data_pre = JSON.parse(fs.readFileSync(file_path))
+                data_pre.push(current)
+                fs.unlinkSync(file_path)
+            }else{
+                var data_pre = [current]
+            }
+            console.log(current)
+            fs.writeFileSync(file_path, JSON.stringify(data_pre))
+    
+            await browser.close();
+            // await sleep(60000);
+            await sleep(6000);
+        } catch (err){
+            console.log(err)
+            continue
         }
-        console.log(current)
-        fs.writeFileSync(file_path, JSON.stringify(data_pre))
 
-        await browser.close();
-        await sleep(60000);
-        // await sleep(6000);
     }
 
 })();
